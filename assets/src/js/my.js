8bdf176b4d1ad1f34789af3f89bfcd25e4b4c5a5
MaxKler <maximumwwe@gmail.com>
2022-01-22 19:38:13 +0200

    g

--- assets/src/js/my.js
+++ assets/src/js/my.js
@@ -84,14 +84,88 @@ toggle.onclick = () => {
 const lazyImages = document.querySelectorAll('img[data-src]')
 const loadMapBlock = document.querySelector('._load-map')
 const windowHeight = document.documentElement.clientHeight
+const loadMoreBlock = document.querySelector('.load__more')
 
 let lazyImagesPositions = []
  if (lazyImages.length > 0) {
 	 lazyImages.forEach(img => {
 		 if (img.dataset.src) {
 			 lazyImagesPositions.push(img.getBoundingClientRect().top + pageYOffset)
+			 lazyScrollCheck()
 		 }
 	 })
  }
 
  console.log(lazyImagesPositions)
+ window.addEventListener("scroll", lazyScroll)
+
+ function lazyScroll() {
+	 if (document.querySelectorAll('img[data-src]').length > 0) {
+		 lazyScrollCheck()
+	 }
+	 if (!loadMapBlock.classList.contains('loaded')) {
+		 getMap()
+	 }
+	 if (!loadMoreBlock.classList.contains('loading')) {
+		getContent()
+	 }
+ }
+
+ function lazyScrollCheck() {
+	 let imgIndex = lazyImagesPositions.findIndex(
+		 item => pageYOffset > item - windowHeight
+		)
+		if (imgIndex >= 0) {
+			if (lazyImages[imgIndex].dataset.src) {
+				lazyImages[imgIndex].src = lazyImages[imgIndex].dataset.src 
+				lazyImages[imgIndex].removeAttribute('data-src')
+			}
+			delete lazyImagesPositions[imgIndex]
+		}
+ }
+
+ function getMap() {
+	 const loadBlockMapPos = loadMapBlock.getBoundingClientRect().top + pageYOffset
+	 if (pageYOffset > loadBlockMapPos - windowHeight) {
+		 const loadMapUrl = loadMapBlock.dataset.map
+		 if (loadMapUrl) {
+			 loadMapBlock.insertAdjacentHTML(
+				 "beforeend",
+				 `<iframe src="${loadMapUrl}"  style="border:0;" allowfullscreen="" loading="lazy"></iframe>`
+			 )
+			 loadMapBlock.classList.add('loaded')
+		 }
+	 }
+ }
+
+ function loadMore() {
+	  const loadMoreBlockPos = loadMoreBlock.getBoundingClientRect().top + pageYOffset
+	  const loadMoreBlockHeight = loadMoreBlock.offsetHeight
+
+	  if (pageYOffset > (loadMoreBlockPos + loadMoreBlockHeight) - windowHeight) {
+		  getContent()
+	  }
+ }
+ async function getContent() {
+	 if (!document.querySelector('.loading__icon')) {
+		 loadMoreBlock.insertAdjacentHTML(
+			 "beforeend",
+			 `<div class="loading__icon"></div>`
+		 )
+	 }
+	 loadMoreBlock.classList.add('loading')
+
+	 let response = await fetch('more.html', {
+		 method: 'GET'
+	 })
+	 if (response.ok) {
+		 let result = await response.text()
+		 loadMoreBlock.insertAdjacentHTML('beforeend', result)
+		 loadMoreBlock.classList.remove('loading')
+		 if (document.querySelector('.loading__icon')) {
+			 document.querySelector('.loading__icon').remove()
+		 } else {
+			 alert('error')
+		 }
+	 }
+ }
